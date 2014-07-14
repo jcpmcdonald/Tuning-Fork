ac5a638ff93c9bf2039a24bbe6bd89dbd201669e
John McDonald <jcpmcdonald@gmail.com>
2014-07-13 18:04:16 -0600

    Adding Low Latency Audio to the suite

--- js/soundTests.js
+++ js/soundTests.js
@@ -24,6 +24,11 @@ var Game = {
 		createjs.Sound.alternateExtensions = ["wav", "ogg"];
 		createjs.Sound.registerSound('water.mp3', 'water');
 		
+		
+		// Howler: https://github.com/goldfire/howler.js
+		this.howlin = new Howl({ urls: ['water.mp3', 'water.ogg', 'water.wav'] });
+		
+		
 		// Using PhoneGap 'Media': http://docs.phonegap.com/en/3.3.0/cordova_media_media.md.html
 		if(typeof Media !== 'undefined'){
 			this.gapMP3 = new Media('water.mp3');
@@ -31,38 +36,12 @@ var Game = {
 			this.gapWAV = new Media('water.wav');
 		}
 		
-		// Howler: https://github.com/goldfire/howler.js
-		this.howlin = new Howl({ urls: ['water.mp3', 'water.ogg', 'water.wav'] });
 		
-		
-		this.start();
-	},
-	
-	start: function() {
-		//if(Game.assetsLoaded && Game.DOMReady)
-		{
-			// alert("HTML Audio");
-			// document.getElementById('htmlAudio').play();
-			// 
-			// alert("Crafty Audio");
-			// Crafty.audio.play("water");
-			// 
-			// alert("SoundJS Audio");
-			// createjs.Sound.play("water");
-			// 
-			// if(typeof Media !== 'undefined'){
-				// alert("PhoneGap MP3");
-				// this.gapMP3.play();
-				// 
-				// alert("PhoneGap OGG");
-				// this.gapOGG.play();
-				// 
-				// alert("PhoneGap WAV");
-				// this.gapWAV.play();
-			// }
-			// 
-			// alert("Howler Audio");
-			// this.howlin.play();
+		// Low Latency Audio plugin for PhoneGap: http://plugins.cordova.io/#/package/com.rjfun.cordova.plugin.lowlatencyaudio
+		if( window.plugins && window.plugins.LowLatencyAudio ) {
+			window.plugins.LowLatencyAudio.preloadFX("water.mp3", "water.mp3");
+			window.plugins.LowLatencyAudio.preloadFX("water.ogg", "water.ogg");
+			window.plugins.LowLatencyAudio.preloadFX("water.wav", "water.wav");
 		}
 	},
 	
